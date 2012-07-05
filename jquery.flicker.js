4fb0956d8da7e480de44289127c2e7f85cac8c05
Nate Hunzaker <nate.hunzaker@gmail.com>
2012-07-05 18:17:52 -0400

    Fixes issues with event binding and adds additional options

--- jquery.flicker.js
+++ jquery.flicker.js
@@ -1,30 +1,48 @@
-// jQuery Flickr
+// jQuery Flicker
+//
 // Causes an element to flicker like an old lightbulb
-// version 0.1, November 1st, 2011
+// Version 0.3, July 5th, 2012
+//
 // by Nate Hunzaker
+// --------------------------------------------------------- //
 
 (function($) {
 
     "use strict";
 
     $.fn.flicker = function(method) {
+		
+		// Default Values
+		// ----------------------------------- //
+		
+		this.defaults = {
+		    minOpacity: 0.7,
+		    maxOpacity: 1,
+		    transition: 200,
+		    delay: 75,
+		    probability: 0.1
+		};
 
+        // Public Methods
+        // ----------------------------------- //
+        
         var methods = {
 
             init : function(options) {
 
-                this.flicker.settings = $.extend({}, this.flicker.defaults, options);
+                var settings = $.extend({}, this.defaults, options);
                 
                 return this.each(function() {
 
                     var $element = $(this), // reference to the jQuery version of the current DOM element
                          element = this;    // reference to the actual DOM element
-                    
-                    $element.data("flicker", {
+                        
+                    $element.data("flicker", $.extend({}, settings, {
                         active: true
-                    });
+                    }));
 
                     methods.flick($element);
+                    
                 });
 
             },
@@ -32,27 +50,43 @@
             // Handle hollogram projection
             // A recursive function to simulate a flickering effect
             flick: function flick ($target) {
-
-                var probability = Math.random(),
-                    transition = Math.random() * 200;
-
+        
+                var options     = $target.data("flicker"),
+                    probability = Math.random(),
+                    transition  = Math.random() * options.transition,
+                    delay       = Math.random() * options.delay,
+                    minOpacity  = 1 - (Math.random() * (1 - options.minOpacity)),
+                    maxOpacity  = 1 - (Math.random() * (1 - options.maxOpacity));
+                
                 // If the 'continue' property of deliberatly set to false, exit
                 if ($target.data("flicker").active === false) {
                     return false;
                 }
 
-                if (probability < 0.1) {
-                    flick($target);
-                } else {
-                    $target.animate({ opacity: Math.random() + 0.7 }, transition).delay(Math.random() * transition).animate({ opacity: 1 }, transition, function() {
+                if (probability > options.probability) {
+                    
+                    setTimeout(function() {
                         flick($target);
+                    }, delay);
+                    
+                } else {
+
+                    $target.animate({ opacity: minOpacity}, transition)
+                           .delay(delay)
+                           .animate({ opacity: maxOpacity }, transition, function() {
+                        
+                        flick($target);
+                        
                     });
+                    
                 }
 
             },
 
             stop: function() {
-                $(this).data("flicker").active = false;
+                this.each(function() {
+                    $(this).text("stopped").data("flicker").active = false;
+                });
             }
 
         };
@@ -66,12 +100,5 @@
         }
 
     };
-
-    $.fn.flicker.defaults = {
-        minOpacity: 0.7,
-        maxOpacity: 1
-    };
-
-    $.fn.flicker.settings = {};
-
+    
 })(window.jQuery);
